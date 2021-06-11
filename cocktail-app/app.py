b890fe3cf6cd1a0f92f05ea975bc1c4cbda38d2f
Tyler <tyler.felsted@yahoo.com>
2021-06-11 17:01:04 -0400

    Added configuration files for heroku

--- cocktail-app/app.py
+++ cocktail-app/app.py
@@ -14,9 +14,11 @@ app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql://postgres:postgres@localhost:5433/cocktail_db'))
+uri = os.environ.get('DATABASE_URL', 'postgresql://postgres:postgres@localhost:5433/cocktail_db')
+if uri.startswith('postgres://'):
+    uri = uri.replace("://", "ql://", 1)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
